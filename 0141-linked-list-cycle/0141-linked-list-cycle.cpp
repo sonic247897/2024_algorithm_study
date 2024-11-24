7579352e70ebe52bb447eaddae25c6aaedc1bc3c
sonic247897 <33999425+sonic247897@users.noreply.github.com>
2024-11-25 03:43:16 +0900

    Time: 7 ms (84.89%), Space: 10.6 MB (96.41%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,7 +11,7 @@ public:
     bool hasCycle(ListNode *head) {
         // 1. cs - 문제에 나온 그대로 next 포인터를 순회하면서 두 번 방문하는 노드가 있는지 확인한다.
         // Follow up: Can you solve it using O(1) (i.e. constant) memory?
-        //  => val을 사용하지 않으므로 visited 플래그 값으로 덮어씌워도 된다. 0, 1은 원본 val이 가질수 있는 값이므로 플래그로 사용하려면 987654321 같은 값을 사용한다.
+        //  => val을 사용하지 않으므로 visited 플래그 값으로 덮어씌워도 된다. 0, 1은 원본 val이 가질수 있는 값이므로 플래그로 사용하려면 987654321 같은 값을 사용한다..
         if (head == NULL) return false;
 
         ListNode *cur = head;
