2ee87cb1326de90244fe297b61c8650130aac2a5
sonic247897 <33999425+sonic247897@users.noreply.github.com>
2024-11-25 19:33:51 +0900

    Time: 0 ms (100%), Space: 10.7 MB (32.53%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        // 1. word 순서대로 저장
+        vector<string> words;
+        int i = 0;
+        while (i < s.size()) {
+            // 앞 공백 제거
+            while (i < s.size() && s[i] == ' ') {
+                i++;
+            }
+            if (i >= s.size()) break; // 맨 끝 공백
+            string tmp = "";
+            while (i < s.size() && s[i] != ' ') {
+                tmp += s[i++];
+            }
+            words.push_back(tmp);
+        }
+        
+        // 2. 뒤에서부터 차례로 word를 뽑아와서 문장 만들기
+        string res = "";
+        for (auto it = words.rbegin(); it != words.rend(); ++it) {
+            res += *it;
+            res += " ";
+        }
+        res.pop_back();
+        
+        return res;
+    }
+};
